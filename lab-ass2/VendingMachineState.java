6eea32fb38b8936b460072148357bb3f2f52fdb9
Lorenzo Nheo Queñano <enzoquenano993@gmail.com>
2026-02-16 17:45:52 +0800

    Untracked lab-ass2 files and now ignored

--- lab-ass2/VendingMachineState.java
+++
@@ -1,6 +0,0 @@
-public interface VendingMachineState {
-    void selectItem(VendingMachine vendingMachine);
-    void insertCoin(VendingMachine vendingMachine, int amount);
-    void dispenseItem(VendingMachine vendingMachine);
-    void setOutOfOrder(VendingMachine vendingMachine);
-}
